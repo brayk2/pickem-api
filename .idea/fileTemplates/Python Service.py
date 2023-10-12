6bdb09dfa85e51b3b35b6fdffe2dd84eea02619e
Brayden Kness <brayk2@gmail.com>
2023-10-12 09:25:42 -0400

    initial commit

--- .idea/fileTemplates/Python Service.py
+++
@@ -1,9 +0,0 @@
-from src.config.base_service import BaseService
-from src.util.injection import inject, dependency
-
-
-@dependency
-class capitalize(${NAME})Service(BaseService):
-    @inject
-    def __init__(self):
-        pass
