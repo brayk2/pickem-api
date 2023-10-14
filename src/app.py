3ef59e66f7d1f6be78435b9609bfde59cfb752a1
Brayden Kness <brayk2@gmail.com>
2023-10-13 20:43:44 -0400

    ping

--- src/app.py
+++ src/app.py
@@ -1,48 +1,22 @@
-import functools
-import logging
-
-import starlette.requests
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from mangum import Mangum
 from starlette.middleware.cors import CORSMiddleware
-
 from src.api.routes import api
+from src.api.routes.ping_router import ping_router
 
-# sql_logger = logging.getLogger("peewee")
-# sql_logger.addHandler(logging.StreamHandler())
-# sql_logger.setLevel(logging.DEBUG)
-
-app = FastAPI(title="Test Api", version="0.0.1", root_path="/")
+app = FastAPI(title="PickEm Api", version="0.0.1", root_path="/")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=True,
 )
-
-
-ping_router = APIRouter(prefix="", tags=["Ping"])
-
-
-@ping_router.get("/")
-def ping():
-    return {"hello": "world"}
 
 
 app.include_router(api)
 app.include_router(ping_router)
 
 
-def dec(f):
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        print(f"args: {args}")
-        print(f"kwargs: {kwargs}")
-        return f(*args, **kwargs)
-
-    return wrapper
-
-
-handler = dec(Mangum(app))
+handler = Mangum(app)
