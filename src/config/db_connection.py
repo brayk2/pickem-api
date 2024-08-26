ea0318162ad15c75bbd1ffd361c8170001066f9d
Brayden Kness <brayk2@gmail.com>
2024-08-25 22:16:06 -0400

    timeout

--- src/config/db_connection.py
+++ src/config/db_connection.py
@@ -1,4 +1,5 @@
 from peewee import PostgresqlDatabase
+from playhouse.pool import PooledPostgresqlDatabase
 
 from src.config.logger import Logger
 from src.config.settings import Settings
@@ -6,11 +7,13 @@ from src.config.settings import Settings
 
 def get_database() -> PostgresqlDatabase:
     settings = Settings()
-    return PostgresqlDatabase(
+    return PooledPostgresqlDatabase(
         settings.db_name,
         host=settings.db_host,
         user=settings.db_user,
         password=settings.db_pass,
+        max_connections=20,  # Maximum number of connections in the pool
+        stale_timeout=300,
         sslmode="require",
     )
 
@@ -19,12 +22,14 @@ logger = Logger()
 
 
 def get_new_database() -> PostgresqlDatabase:
-    logger.info("getting database connection")
     settings = Settings()
-    return PostgresqlDatabase(
+    return PooledPostgresqlDatabase(
         settings.db_name,
         host=settings.db_host,
         user=settings.db_user,
         password=settings.db_pass,
+        max_connections=20,
+        stale_timeout=None,
+        connection_timeout=None,
         sslmode="require",
     )
